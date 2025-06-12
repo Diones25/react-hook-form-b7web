3b7a2f70c10707e8fefb513d45744b06b29fb26f
Diones <pereiradiones987@gmail.com>
2025-06-12 12:01:24 -0300

    Lidando com erro de validação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,11 @@ import { Inputs } from '../types/Inputs';
 
 
 const Home = () => {
-  const { handleSubmit, register } = useForm<Inputs>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm<Inputs>();
 
   const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data)
@@ -18,9 +22,30 @@ const Home = () => {
           <div className='my-3'>
             <input
               {...register('name', { required: true, minLength: 3, maxLength: 20 })}
-              className='border border-white p-3 text-black'
+              className={`border ${errors.name ? 'border-red-600' : 'border-white'} p-3 text-black`}
               placeholder='Digite seu nome'
             />
+            {errors.name?.type === 'required' &&
+              <p
+                className='text-red-600 text-sm'
+              >
+                Item obrigatório
+              </p>
+            }
+            {errors.name?.type === 'minLength' &&
+              <p
+                className='text-red-600 text-sm'
+              >
+                Precisar ter 3 caracteres
+              </p>
+            }
+            {errors.name?.type === 'maxLength' &&
+              <p
+                className='text-red-600 text-sm'
+              >
+                Precisar ter no máximo 20 caracteres
+              </p>
+            }
           </div>
 
           <div className='my-3'>
@@ -29,15 +54,37 @@ const Home = () => {
               className='border border-white p-3 text-black'
               placeholder='Digite seu sonrenome'
             />
+            
           </div>
 
           <div className='my-3'>
             <input
             type='number'
-              {...register('age', { min: 18, max: 120 })}
+              {...register('age', { required: 'campo idade obrigatório', min: 18, max: 120 })}
               className='border border-white p-3 text-black'
               placeholder='Digite sua idade'
             />
+            {errors.age &&
+              <p
+                className='text-red-600 text-sm'
+              >
+                {errors.age.message}
+              </p>
+            }
+            {errors.age?.type === 'min' &&
+              <p
+                className='text-red-600 text-sm'
+              >
+                Idade precisa ser maior que 18
+              </p>
+            }
+            {errors.age?.type === 'max' &&
+              <p
+                className='text-red-600 text-sm'
+              >
+                Idade precisa ser menor que 120
+              </p>
+            }
           </div>
 
           <input
